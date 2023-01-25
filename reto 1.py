14abe46c98d63cc105e8a6d71de39266eb2010f3
Brian Alexis Paredes Andrade <68014465+bparedes21@users.noreply.github.com>
2023-01-25 14:53:34 -0300

    reto 1

--- reto 1.py
+++ reto 1.py
@@ -1,8 +1,15 @@
 """Reto 1
+
+Lenguaje: Python
+IDE recomendado: PyCharm
+Nivel: Inicial
 Enunciado: Dado un fichero excel con nombres y correos (columna nombre y columna email), realiza un script en Python que devuelva los mails únicos de la columna email.
 Consideraciones: Utiliza la librería pandas para procesar el fichero Excel (.xls).
 
 """
+#utilice PyCharm
+#utilice la librería pandas
+#utilice el fichero Excel
 
 def return_mails(Excel_df):
     mails_unique = Excel_df.email.drop_duplicates()
